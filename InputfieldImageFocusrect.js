cb01c30a034b177b6b4b5744a361a1f5bd1c4261
klickstarter <klickstarter@b31.net>
2014-10-26 12:40:47 +0100

    first commit

---
+++ InputfieldImageFocusrect.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+
+    $('.InputfieldImageFocusrect').on('click', '.button-setfocus', function (event) {
+
+        event.preventDefault();
+
+        var popup;
+        var $saveButton;
+        var $cancelButton;
+        var jcrop;
+        var $targetInput = $('#' + $(this).data('inputtarget'));
+        var $container = $(this).parents('.InputfieldImage');
+        var imageUrl = $container.find('a.InputfieldFileLink').attr('href');
+
+        function initCropping() {
+            $('#image-focusrect-modal').css('max-width', '90%');
+            var jcropSettings = {
+                boxWidth: $(window).width() * .9,
+                boxHeight: ($(window).height() * .9) - 60
+            };
+            var savedCoords = $targetInput.val();
+            if (savedCoords) {
+                savedCoords = JSON.parse(savedCoords);
+                jcropSettings.setSelect = [savedCoords.x, savedCoords.y, savedCoords.x2, savedCoords.y2]
+            }
+
+            $('#target').Jcrop(jcropSettings, function () {
+                jcrop = this;
+            });
+
+        };
+
+        $.magnificPopup.open({
+            type: 'ajax',
+            closeOnContentClick: false,
+            closeBtnInside: false,
+            items: {
+                src: ['<div id="image-focusrect-modal">',
+                    '<img src="'+ imageUrl +'" id="target" />',
+                    '<button class="ui-button ui-state-default image-focuspoint-save">Apply</button>',
+                    '<button class="ui-button ui-state-default image-focuspoint-cancel">Cancel</button>',
+                    '</div>'
+                ].join('\n'),
+                type: 'inline'
+            },
+            callbacks: {
+                open: function () {
+                    popup = this;
+                    $saveButton = $(this.content).find('.image-focuspoint-save');
+                    $cancelButton = $(this.content).find('.image-focuspoint-cancel');
+
+                    $saveButton.click(function (e) {
+                        var saveVal = jcrop.tellSelect();
+                        if (saveVal.w === 0 && saveVal.h === 0) {
+                            saveVal = '';
+                        } else {
+                            saveVal = JSON.stringify(saveVal);
+                        }
+                        $targetInput.val(saveVal);
+                        jcrop.destroy();
+                        popup.close();
+                    });
+
+                    $cancelButton.click(function (e) {
+                        jcrop.destroy();
+                        popup.close();
+                    });
+
+                    initCropping()
+                }
+            }
+        });
+
+    });
+
+
+});
---
+++ README.md
@@ -0,0 +1,33 @@
+Inputfield And Fieldtype ImageFocusrect
+=======================================
+
+for ProcessWire 2.5
+-------------------
+
+Makes it possible to select the important part of the image
+
+Usage
+-----
+
+Create a new Inputfield ImageFocusrect and add it to the desired template. 
+Edit a page with this template and add an image to the new field. You will see a link "Add Image Focusarea". 
+Click the link and you will see a popup with a cropping tool similar to the Thumbnails module. Select the important part of the image, click "apply" and save the page.
+
+By default the Field hooks into Pageimage::size and automatically populates the cropping option with percentages of the center of the selected focusarea. 
+
+API usage examples
+------------------
+
+```PHP
+// by default if you did not define a cropping option, the cropping option gets automatically populated with percentages
+echo "<img src='{$page->image->size(200,200)}' />";
+
+// the same as above but here you explict say you want this behaviour
+echo "<img src='{$page->image->size(200,200, array('cropping'=>'align'))}' />";
+
+// here the resulting cropped image will be the center area of the selected focusarea 
+echo "<img src='{$page->image->size(200,200, array('cropping'=>'inside'))}' />";
+
+// here the complete focusarea will be part of the image, but the outside part will still be used
+echo "<img src='{$page->image->size(200,200, array('cropping'=>'outside'))}' />";
+```
