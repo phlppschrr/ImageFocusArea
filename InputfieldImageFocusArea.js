12521faafaf4795f61f21df61dbb6220ebb7359c
klickstarter <klickstarter@b31.net>
2014-10-29 12:03:17 +0100

    Fix to enable grip mode toggeling on page edit screen
    Using modified code found in the thumbanils support thread here:
    https://processwire.com/talk/topic/643-release-thumbnails/page-16#entry5
    2117
    Thanks go to user iNoize

--- InputfieldImageFocusArea.js
+++ InputfieldImageFocusArea.js
@@ -74,5 +74,45 @@ $(document).ready(function () {
 
     });
 
+// Grid Configurations
+    function setGridMode($parent) {
+        $parent.find("i.fa-th").replaceWith($("<i class='fa fa-list'></i>"));
+        $parent.find(".InputfieldFileLink").each(function() {
+            var $a = $(this);
+            var $img = $a.children("img");
+            $a.css('background', '#000 url(' + $img.attr('src') + ') center center no-repeat');
+            if($img.width() > 99 && $img.height() > 99) $a.css('background-size', 'cover');
+        });
+        $parent.addClass('InputfieldImageGrid');
+    }
 
+    function unsetGridMode($parent) {
+        $parent.removeClass('InputfieldImageGrid');
+        $parent.find(".InputfieldFileLink").css('background', 'none');
+        $parent.find("i.fa-list").replaceWith($("<i class='fa fa-th'></i>"));
+    }
+
+    var $listToggle = $("<a class='InputfieldImageListToggle HideIfSingle HideIfEmpty' href='#'></a>")
+        .append("<i class='fa fa-th'></i>");
+    $(".InputfieldImageFocusArea .InputfieldHeader").append($listToggle);
+    $(document).on('click', '.InputfieldImageListToggle', function() {
+        var $parent = $(this).parents(".InputfieldImageFocusArea");
+        if($parent.hasClass('InputfieldImageGrid')) unsetGridMode($parent);
+        else setGridMode($parent);
+        return false;
+    });
+
+    $(".InputfieldImageFocusArea").find(".InputfieldImageDefaultGrid").each(function() {
+        setGridMode($(this).parents(".InputfieldImageFocusArea"));
+    });
+
+
+    $(".InputfieldCropImage .InputfieldFileList").live('AjaxUploadDone', function() {
+        $("a.InputfieldFileLink", $(this)).fancybox();
+
+        // NEW Check for default Settings for Image View
+        var $parent = $(this).parents('.InputfieldGridImage');
+        if($parent.is(".InputfieldImageGrid")) setGridMode($parent);
+
+    });
 });
