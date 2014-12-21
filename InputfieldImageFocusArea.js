144e537e25adab60d662af5bf0d31ecf1cc0e0a5
klickstarter <klickstarter@b31.net>
2014-12-21 19:01:37 +0100

    Fix for Gridview, template option hideFocusArea

--- InputfieldImageFocusArea.js
+++ InputfieldImageFocusArea.js
@@ -45,7 +45,6 @@ $(document).ready(function () {
             if (aspectRatio) {
                 jcropSettings.aspectRatio = aspectRatio;
             }
-                console.log('change listner setiu', ratioWidth, ratioHeight);
             if (ratioWidth>0 || ratioHeight>0) {
                 jcropSettings.onChange = function (c) {
                     // change marker if selection would need upscaling in thumbmode
@@ -117,45 +116,99 @@ $(document).ready(function () {
 
     });
 
-    // Grid Configurations
+});
+
+
+$(document).ready(function() {
+
+    var magnificOptions = {
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: true,
+        image: {
+            titleSrc: function(item) {
+                return item.el.find('img').attr('alt');
+            }
+        },
+        callbacks: {
+            open: function() {
+                // for firefox, which launches Magnific after a sort
+                if($(".InputfieldFileJustSorted").size() > 0) this.close();
+            }
+        }
+    };
+
+    $("a.InputfieldFileLink").magnificPopup(magnificOptions);
+
+    $(document).on('click', '.InputfieldImageFocusArea .InputfieldFileMove', function() {
+
+        var $li = $(this).parent('p').parent('li');
+        var $ul = $li.parent();
+
+        if($(this).is('.InputfieldFileMoveTop')) $ul.prepend($li);
+        else $ul.append($li);
+
+        $ul.children('li').each(function(n) {
+            $(this).find('.InputfieldFileSort').val(n);
+        });
+
+        return false;
+    });
+
+    function setGridModeItem($a) {
+        var $img = $a.children("img");
+        var gridSize = $img.attr('data-gridsize');
+        $a.css('background', '#000 url(' + $img.attr('src') + ') center center no-repeat');
+        $a.css('width', gridSize + 'px').css('height', gridSize + 'px');
+        $img.css('height', gridSize + 'px');
+        if($img.width() > (gridSize-1) && $img.height() > (gridSize-1)) $a.css('background-size', 'cover');
+    }
+
     function setGridMode($parent) {
         $parent.find("i.fa-th").replaceWith($("<i class='fa fa-list'></i>"));
-        $parent.find(".InputfieldFileLink").each(function () {
-            var $a = $(this);
-            var $img = $a.children("img");
-            $a.css('background', '#000 url(' + $img.attr('src') + ') center center no-repeat');
-            if ($img.width() > 99 && $img.height() > 99) $a.css('background-size', 'cover');
+        $parent.find(".InputfieldFileLink").each(function() {
+            setGridModeItem($(this));
         });
         $parent.addClass('InputfieldImageGrid');
     }
 
+    function unsetGridModeItem($a) {
+        $a.css({
+            background: 'none',
+            width: '',
+            height: ''
+        }).find('img').css('height', '');
+    }
+
     function unsetGridMode($parent) {
         $parent.removeClass('InputfieldImageGrid');
-        $parent.find(".InputfieldFileLink").css('background', 'none');
+        $parent.find(".InputfieldFileLink").each(function() {
+            unsetGridModeItem($(this));
+        });
         $parent.find("i.fa-list").replaceWith($("<i class='fa fa-th'></i>"));
     }
 
-    var $listToggle = $("<a class='InputfieldImageListToggle HideIfSingle HideIfEmpty' href='#'></a>")
-            .append("<i class='fa fa-th'></i>");
+    var $listToggle = $("<a class='InputfieldImageListToggle HideIfEmpty' href='#'></a>")
+        .append("<i class='fa fa-th'></i>");
     $(".InputfieldImageFocusArea .InputfieldHeader").append($listToggle);
-    $(document).on('click', '.InputfieldImageListToggle', function () {
+    $(document).on('click', '.InputfieldImageListToggle', function() {
         var $parent = $(this).parents(".InputfieldImageFocusArea");
-        if ($parent.hasClass('InputfieldImageGrid')) unsetGridMode($parent);
+        if($parent.hasClass('InputfieldImageGrid')) unsetGridMode($parent);
         else setGridMode($parent);
         return false;
     });
 
-    $(".InputfieldImageFocusArea").find(".InputfieldImageDefaultGrid").each(function () {
-        setGridMode($(this).parents(".InputfieldImageFocusArea"));
+    $(".InputfieldImageFocusArea").find(".InputfieldImageDefaultGrid").each(function() {
+        setGridMode($(this).parents(".InputfieldImage"));
     });
 
-
-    $(".InputfieldCropImage .InputfieldFileList").live('AjaxUploadDone', function () {
-        $("a.InputfieldFileLink", $(this)).fancybox();
-
-        // NEW Check for default Settings for Image View
-        var $parent = $(this).parents('.InputfieldGridImage');
-        if ($parent.is(".InputfieldImageGrid")) setGridMode($parent);
-
+    $(document).on('AjaxUploadDone', '.InputfieldImageFocusArea .InputfieldFileList', function() {
+        $("a.InputfieldFileLink", $(this)).magnificPopup(magnificOptions);
+        var $parent = $(this).parents('.InputfieldImageFocusArea');
+        if($parent.is(".InputfieldImageGrid")) {
+            unsetGridMode($parent);
+            setGridMode($parent);
+        }
     });
+
 });
